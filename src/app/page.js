e666c6f784445539a96dbe1f2ad71ebb6a9dbc8f
gileck <gileck@wix.com>
2024-04-07 18:12:16 +0300

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,42 +1,69 @@
 "use client"
 import React from "react";
 import styles from "./page.module.css";
-import { Card, Collapse, } from "@mui/material";
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Divider from '@mui/material/Divider';
-import InboxIcon from '@mui/icons-material/Inbox';
-import AddIcon from '@mui/icons-material/Add';
 import { AppTabs } from './tabs.js';
 import { EditPlan } from './components/EditPlan.js';
-import { AddExercise } from "./components/addExercise";
-import { Add } from "@mui/icons-material";
 import dynamic from 'next/dynamic'
 import { TrainingPlan } from "./components/TrainingPlan";
-import _ from 'lodash'
-import { localStorageAPI } from "./localStorageAPI";
 import { Settings } from "./components/Settings";
 import { Workout } from "./components/Workout";
-
-const { getData, saveData, removeData } = localStorageAPI();
-
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import {Paper} from "@mui/material";
+import SettingsIcon from '@mui/icons-material/Settings';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import NoteAddIcon from '@mui/icons-material/NoteAdd';
 export default function Home() {
+
   const ClientTabs = dynamic(() => Promise.resolve(AppTabs), { ssr: false })
+    const [value, setValue] = React.useState(2);
+    console.log({value})
+
+    const Comps = [
+        { label: "Workout", Comp: Workout, icon: <FitnessCenterIcon /> },
+        { label: "Training Plan", Comp: TrainingPlan , icon: <FormatListBulletedIcon />  },
+        { label: "Edit Plan", Comp: EditPlan, icon: <NoteAddIcon />  },
+        { label: "Settings", Comp: Settings , icon: <SettingsIcon />  },
+    ]
+
+    const CompToRender = Comps[value].Comp
 
   return (
     <main className={styles.main}>
-      <ClientTabs
-          initialState={2}
-        Comps={
-          [
-            { label: "Workout", Comp: <Workout /> },
-            { label: "Training Plan", Comp: <TrainingPlan /> },
-            { label: "Edit Plan", Comp: <EditPlan /> },
-            { label: "Settings", Comp: <Settings /> },
-          ]}
-      />
+        <CompToRender />
+
+        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+            <SimpleBottomNavigation
+                onChange={(event, newValue) => setValue(newValue)}
+                Comps={Comps}
+                value={value}
+            />
+        </Paper>
 
     </main>
   );
 }
 
+ function SimpleBottomNavigation({ Comps, onChange, value }) {
+
+    return (
+        <Box sx={{ maxWidth: 380 }}>
+            <BottomNavigation
+                showLabels
+                value={value}
+                onChange={onChange}
+            >
+                {
+                    Comps.map(({ label, icon }, index) => (
+                        <BottomNavigationAction label={label} icon={icon || <SettingsIcon/> } />
+                    ))
+                }
+            </BottomNavigation>
+        </Box>
+    );
+}
