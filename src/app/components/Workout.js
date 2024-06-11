595cff986badb531f2746f27415ac20f41ba47d0
Gil <gil@Gils-MacBook-Pro.local>
2024-06-11 15:40:47 +0300

    c

--- src/app/components/Workout.js
+++ src/app/components/Workout.js
@@ -233,14 +233,16 @@ export function WorkoutList({
     const weekOpened = shouldKeepCurrentWeekOpened ? currentWeekOpened : firstWeekWithExercisesLeft
 
     const [selectedWeek, setSelectedWeek] = useState(weekOpened || 0)
-    const firstWorkoutWithExercisesLeft = workouts.find(w => w.exercises.some(e => e.weeks[firstWeekWithExercisesLeft].totalWeeklySets < e.weeks[firstWeekWithExercisesLeft].weeklyTarget))
+    // const firstWorkoutWithExercisesLeft = workouts.find(w => w.exercises.some(e => e.weeks[firstWeekWithExercisesLeft].totalWeeklySets < e.weeks[firstWeekWithExercisesLeft].weeklyTarget))
     const totalSetsThisWeek = workouts.reduce((acc, w) => acc + w.exercises.reduce((acc, e) => acc + (e.weeks[selectedWeek].totalWeeklySets || 0), 0), 0)
     const thisWeekSetsTarget = workouts.reduce((acc, w) => acc + w.exercises.reduce((acc, e) => acc + (e.weeks[selectedWeek].weeklyTarget || 0), 0), 0)
     const isWeekDone = totalSetsThisWeek >= thisWeekSetsTarget
 
-    const [openWorkouts, setOpenWorkouts] = useState({
-        [firstWorkoutWithExercisesLeft?.id]: true
-    });
+    // const [openWorkouts, setOpenWorkouts] = useState({
+    //     [firstWorkoutWithExercisesLeft?.id]: true
+    // });
+
+    const [openWorkouts, setOpenWorkouts] = useState({});
 
     function onSelectedWeekChanges(newWeek) {
         setSelectedWeek(newWeek)
