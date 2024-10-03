53e0751e2b194f329f86714eb07d895d506140d2
Gil <gil@Gils-MacBook-Pro.local>
2024-10-03 11:53:39 +0300

    commit

--- src/app/home.js
+++ src/app/home.js
@@ -148,12 +148,10 @@ export function Home({ user }) {
   }
 
   useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
-          .then(reg => console.log('Service Worker registered:', reg))
-          .catch(err => console.error('Service Worker registration failed:', err));
-      });
+    if (navigator.serviceWorker) {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(reg => console.log('Service Worker registered:', reg))
+        .catch(err => console.error('Service Worker registration failed:', err));
     }
   }, []);
 
