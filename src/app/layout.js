6a4d888ed9b6880c6f2461c46830eb6f7f1f481e
Gil <gil@Gils-MacBook-Pro.local>
2024-10-04 12:29:07 +0300

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" sizes="180x180" href="/favicon.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
       <body className={inter.className}>{children}</body>
     </html>
   );
