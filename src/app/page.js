44904af5ee8d0759794a1a971137798d6b17b74d
Gil <gil@Gils-MacBook-Pro.local>
2024-11-02 12:12:03 +0200

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -5,32 +5,10 @@ import { useFetch } from '@/useFetch';
 import dynamic from 'next/dynamic'
 
 
-// const baseUrl = process.env.NODE_ENV === 'production' ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}` : 'http://localhost:3000';
-// console.log({ baseUrl });
-
 const HomeLazy = dynamic(() => import('./home').then(m => m.Home), { ssr: false })
 
 export default function () {
-    // const cookieStore = cookies()
-    // const cookie = cookieStore.get('key')
-    // const userKey = cookie?.value
-    // if (!userKey) {
-    //     return <Client />
-    // }
 
-    // fetch(`${baseUrl}/api/enter`, {
-    //     headers: {
-    //         cookie: `key=${userKey}`
-    //     }
-    // })
-
-    // const plansFromLocalStorage = localStorageAPI().getData('trainingPlans')
-
-
-
-    // const [user, setUser] = useState(null);
-    // const [loading, setLoading] = useState(true);
-    // const [error, setError] = useState(null);
 
     const { data, loading } = useFetch('/api/user')
     const user = data?.user
